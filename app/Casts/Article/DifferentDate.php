78da64f4d0569f92e355e79c239d98ccd83a279f
bklv-kirill <bklv.kirill@yandex.ru>
2025-03-23 15:11:38 -0700

    Add strict types declare

--- app/Casts/Article/DifferentDate.php
+++ app/Casts/Article/DifferentDate.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Casts\Article;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
--- app/Http/Controllers/Category/Pages/ShowController.php
+++ app/Http/Controllers/Category/Pages/ShowController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Category\Pages;
 
 use App\Http\Controllers\Controller;
