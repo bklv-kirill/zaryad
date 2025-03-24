afc5ad584ae6bee5001775e7238ef675fed90164
bklv-kirill <bklv.kirill@yandex.ru>
2025-03-24 11:55:37 -0700

    Add dependencies injection
    Добавил внедрение зависимостей.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bindIf(
+            abstract: \App\Interfaces\Repositories\Category\CategoryRepository::class,
+            concrete: \App\Repositories\Category\LaravelCategoryRepository::class);
+        $this->app->bindIf(
+            abstract: \App\Interfaces\Repositories\Article\ArticleRepository::class,
+            concrete: \App\Repositories\Article\LaravelArticleRepository::class,
+        );
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
     }
 }
